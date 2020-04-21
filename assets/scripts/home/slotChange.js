b0aa090b46ed850f2ef1e87848d7cc2a28ec1a38
Wangzihao5325 <416990273@qq.com>
2020-04-21 09:54:56 +0800

    1.normal merge

---
+++ assets/scripts/home/slotChange.js
@@ -0,0 +1,69 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        ske_anim: {
+            type: sp.Skeleton, // 
+            default: null,
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    headSlotChange() {
+        var spine = this.ske_anim;
+        var ske_com = spine.getComponent(sp.Skeleton);
+        this.ske_com = ske_com;
+        const skeletonData = this.ske_com.skeletonData.getRuntimeData();
+        const skin = skeletonData.findSkin('default');
+
+        let parts = ["toushi00", "boshi00", "weishi00"];
+        let randomNum = Math.floor(Math.random() * 8);
+        for (let i = 0; i < parts.length; i++) {
+            let regSlot = this.ske_com.findSlot(parts[i]);
+            console.log(parts[i]);
+            console.log(regSlot);
+            switch (i) {
+                case 0:
+                    {
+                        let slotIndex = skeletonData.findSlotIndex(`toushi0${randomNum}`);
+                        let atta = skin.getAttachment(slotIndex, `toushi0${randomNum}`);
+                        console.log(atta);
+                        regSlot.setAttachment(atta);
+                        break;
+                    }
+                case 1:
+                    {
+                        let slotIndex = skeletonData.findSlotIndex(`boshi0${randomNum}`);
+                        let atta = skin.getAttachment(slotIndex, `boshi0${randomNum}`);
+                        console.log(atta);
+                        regSlot.setAttachment(atta);
+                        break;
+                    }
+                case 2:
+                    {
+                        let slotIndex = skeletonData.findSlotIndex(`weishi0${randomNum}`);
+                        let atta = skin.getAttachment(slotIndex, `weishi0${randomNum}`);
+                        console.log(atta);
+                        regSlot.setAttachment(atta);
+                        break;
+                    }
+            }
+        }
+    },
+
+    start() {
+
+    },
+
+    // update (dt) {},
+});
