396be658bb6f9ec0ed43643239bed72aba22ca7c
louis <louislai@qq.com>
2020-04-29 10:36:55 +0800

    动态导入mark  spriteFrame

--- assets/scripts/game/item_index.js
+++ assets/scripts/game/item_index.js
@@ -1,4 +1,4 @@
-const MASKSIZE = [[0, 0, 406, 351], [0,0,341,353], [0,0,462,431],[0,0,355,276],[0,0,399,362],[0,0,342,369]];//01图片尺寸 23图片起始点坐标
+// const MASKSIZE = [[0, 0, 406, 351], [0,0,341,353], [0,0,462,431],[0,0,355,276],[0,0,399,362],[0,0,342,369]];//01图片尺寸 23图片起始点坐标
 
 cc.Class({
     extends: cc.Component,
@@ -7,13 +7,6 @@ cc.Class({
         label_num: cc.Label,
         sp_item: cc.Sprite,
         mask_item: cc.Mask,
-        spframe_mask1: cc.SpriteFrame,
-        spframe_mask2: cc.SpriteFrame,
-        spframe_mask3: cc.SpriteFrame,
-        spframe_mask4: cc.SpriteFrame,
-        spframe_mask5: cc.SpriteFrame,
-        spframe_mask6: cc.SpriteFrame,
-
     },
 
     init(num) {
@@ -25,28 +18,17 @@ cc.Class({
         this.sp_item.spriteFrame = spt
     },
 
-    defaultRect(spframe_mask,index) {
-
-        let rect = new cc.Rect(MASKSIZE[index][0], MASKSIZE[index][1], MASKSIZE[index][2], MASKSIZE[index][3]);
-        let spframe_puzzle_clone = spframe_mask.clone();
-        spframe_puzzle_clone.setRect(rect);
-        return spframe_puzzle_clone
-    },
-
-
-    setMarsk(index,item){
-        var urls = ['2x3-1/1', '2x3-1/2'];
+    setMarsk(index){
+        var urls = ['2x3-1/1', '2x3-1/2','2x3-1/3', '2x3-1/4','2x3-1/5', '2x3-1/6'];
+        const self = this;
         cc.loader.loadResArray(urls, cc.SpriteFrame, function (err, assets) {
             if (err) {
                 cc.error(err);
                 return;
             }
             console.log("assets",assets)
-            // spriteFrames = assets;
-            // ...
+            self.mask_item.spriteFrame = assets[index]
         });
-        const spframList=[this.spframe_mask1,this.spframe_mask2,this.spframe_mask3,this.spframe_mask4,this.spframe_mask5,this.spframe_mask6]
-        this.mask_item.spriteFrame =this.defaultRect(spframList[index],index)
     },
 
     setTouch() {
