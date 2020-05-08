7d01141a3a886f4c1f99f423930cb5a1c05acfb5
Wangzihao5325 <416990273@qq.com>
2020-05-08 15:14:39 +0800

    1.引入6*8切片资源 2.优化切片蒙版部分代码

--- assets/scripts/game/game_test.js
+++ assets/scripts/game/game_test.js
@@ -1,15 +1,4 @@
-// Learn cc.Class:
-//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
-const SIZES_0 = [[0, 0, 406, 351, -119, 253], [303, 0, 341, 353, 151.5, 252], [0, 351 - 82, 333, 310, -155.5, 4.5], [235, 353 - 87, 409, 317, 117.5, 4], [0, 857 - 362, 399, 362, -122.5, -247.5], [644 - 342, 857 - 369, 342, 369, 151, -244]];
-
-const TYPES = [[2, 3], [4, 6], [6, 8]];
-const BG_WIDTH = 644;
-const BG_HEIGHT = 857;
+import { SIZES } from '../global/piece_index';
 
 cc.Class({
     extends: cc.Component,
@@ -19,65 +8,43 @@ cc.Class({
         pre_item: cc.Prefab,
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
-    onLoad () {
-        this.game_bg.zIndex=1
+    onLoad() {
         this.init();
     },
 
-    init(){
+    init() {
+        this.game_bg.zIndex = 1;
         this.initItem()
     },
 
-    initItem(typeKey=0) {
-        //根据key取type
-        let type = TYPES[typeKey];
-        //根据type取size
-        let sizeArr = SIZES_0;
-        //一负一正是为了块排列顺序与切割顺序一致
-        let x_start = -1 * Math.floor(BG_WIDTH / 2);
-        let y_start = Math.floor(BG_HEIGHT / 2);
-        let widthSeparate = Math.floor(BG_WIDTH / type[0] / 2);
-        let heightSeparate = Math.floor(BG_HEIGHT / type[1] / 2);
-        //图片切割起点积累量
-        let dy = 0;
-        let dx = 0;
-        //遍历size根据size生成item
+    initItem(hardLevel = 0) {// hardLevel: 0->2*3; 1->4*6; 2->6*8
+        /*根据难度获取切片数据数组*/
+        let sizeArr = SIZES[hardLevel];
+        /*遍历sizeArr生成item*/
         sizeArr.forEach((item, index) => {
+            /*根据预制资源实例化节点*/
             let item_node = cc.instantiate(this.pre_item);
-            item_node.name=`item_puzzle_warp-${index+1}`
+            /*设置节点属性*/
+            item_node.name = `item_puzzle_warp-${index + 1}`
             item_node.width = item[2];
             item_node.height = item[3];
-            // console.log("item_node.getComponent('item_puzzle')",item_node.getComponent('item_puzzle'))
             item_node.getChildByName('item_puzzle').width = item[2];
             item_node.getChildByName('item_puzzle').height = item[3];
             item_node.parent = this.game_bg;
-            let y_index = Math.floor(index / type[0]);
-            let x_index = index % type[0];
-            //一负一正是为了块排列顺序与切割顺序一致
-    
-            item_node.setPosition(item[4],item[5]);
-            item_node.zIndex=10;
-            // if(index===0){
-            //     item_node.active=false
-            // }
+            item_node.setPosition(item[4], item[5]);
+            item_node.zIndex = 10;
+            /*获取itembg_index对象*/
             let obj = item_node.getComponent('itembg_index');
-            console.log('obj',obj)
             if (obj) {
-                obj.init(index); 
-                // obj.setSpItem(this.defaultRect(item));
-                obj.setMarsk(index)
+                obj.setMarsk(index, hardLevel)
+                /*在对象中保存节点引用，便于后续调用*/
                 obj.item_node = item_node;
             }
         });
     },
 
-
-
-    start () {
+    start() {
 
     },
 
-    // update (dt) {},
 });
--- assets/scripts/game/itembg_index.js
+++ assets/scripts/game/itembg_index.js
@@ -1,62 +1,26 @@
-// const MASKSIZE = [[0, 0, 406, 351], [0,0,341,353], [0,0,462,431],[0,0,355,276],[0,0,399,362],[0,0,342,369]];//01图片尺寸 23图片起始点坐标
+import { MASK_RESOUSE } from '../global/piece_index';
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // label_num: cc.Label,
-        // sp_item: cc.Sprite,
         mask_item: cc.Mask,
     },
 
-
-    init(num) {
-        // this.num = num;
-        // this.label_num.string = this.num
-    },
-
-
-    setMarsk(index){
-        console.log("setMarsk")
-        var urls = ['2x3-1/1', '2x3-1/2','2x3-1/3', '2x3-1/4','2x3-1/5', '2x3-1/6'];
+    setMarsk(index, hardLevel) {
         const self = this;
-        cc.loader.loadResArray(urls, cc.SpriteFrame, function (err, assets) {
+        let urls = MASK_RESOUSE[hardLevel];
+        cc.loader.loadRes(urls[index], cc.SpriteFrame, function (err, assets) {
             if (err) {
                 cc.error(err);
                 return;
             }
-            console.log("assets",assets)
-            self.mask_item.spriteFrame = assets[index]
+            self.mask_item.spriteFrame = assets;
         });
-    },
-
-    setTouch() {
-        this.node.on(cc.Node.EventType.TOUCH_START, () => {
-            this.item_node.zIndex= 10//拿起增加z-index
-        })
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
-            // console.log("evnt",event)
-            if (this.item_node) {
-                let delta = event.touch.getDelta();
-                let newPositin = cc.v2(this.item_node.x + delta.x, this.item_node.y + delta.y)
-                this.item_node.setPosition(newPositin);
-            }
-        })
-        this.node.on(cc.Node.EventType.TOUCH_END, () => {
-            this.item_node.zIndex= 1//恢复z-index
-        })
-    },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    onLoad() {
-        // this.num = 0;
-        // this.setTouch();
     },
 
     start() {
 
     },
 
-    // update (dt) {},
 });
