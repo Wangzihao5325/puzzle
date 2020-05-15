630b4c68e1fde730a67feb26438b9bfb23e72e83
Wangzihao5325 <416990273@qq.com>
2020-05-15 14:59:49 +0800

    1.vistor动画

--- assets/scripts/show/show_index.js
+++ assets/scripts/show/show_index.js
@@ -7,6 +7,7 @@ cc.Class({
     properties: {
         root: cc.Node,
         footer: cc.Prefab,
+        vistor: cc.Prefab,
     },
 
     stateUpdate() {
@@ -19,11 +20,63 @@ cc.Class({
         obj.initWithScene(CACHE.scene);
         footer.parent = this.root;
         footer.setPosition(0, -500);
+        footer.zIndex = 10;
     },
 
+    /*
+    payload={
+        startPosition:[x,y],
+        pausePosition:[x,y],
+        endPosition:[x,y],
+        //  period必须为2的整数倍
+        firstPeriod:number,
+        secondPeriod:number,
+        pausePeriod:number,
+    }
+    */
+    vistorInit(payload) {
+        let vistor = cc.instantiate(this.vistor);
+        let obj = vistor.getComponent('vistor_index');
+        vistor.parent = this.root;
+        vistor.setPosition(payload.startPosition[0], payload.startPosition[1]);
+        let firstDriection = payload.pausePosition[0] - payload.startPosition[0];
+        let secondDriection = payload.endPosition[0] - payload.pausePosition[0];
+        /*动画*/
+        cc.tween(vistor)
+            .call(() => {
+                obj.init(firstDriection);
+                obj.animateStart(payload);
+            })
+            .to(payload.firstPeriod, { position: cc.v2(payload.pausePosition[0], payload.pausePosition[1]) })
+            .call(() => {
+                obj.manWait();
+            })
+            .delay(payload.pausePeriod)
+            .call(() => {
+                obj.initMan(secondDriection);
+            })
+            .to(payload.secondPeriod, { position: cc.v2(payload.endPosition[0], payload.endPosition[1]) })
+            .start()
+    },
+
+    randomCreateVistor(){
+
+    },
+
+
+
     init() {
+        let payload = {
+            startPosition: [0, -400],
+            pausePosition: [-200, -400],
+            endPosition: [400, -400],
+            firstPeriod: 2,
+            secondPeriod: 6,
+            pausePeriod: 2
+        }
         this.stateUpdate();
         this.footerInit();
+        this.vistorInit(payload);
     },
 
     // LIFE-CYCLE CALLBACKS:
