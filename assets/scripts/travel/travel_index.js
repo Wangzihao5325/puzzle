aec8a0327d37146ed6096a6147d93c46250e186c
Wangzihao5325 <416990273@qq.com>
2020-05-18 15:33:21 +0800

    1.画虚线

--- assets/scripts/travel/travel_index.js
+++ assets/scripts/travel/travel_index.js
@@ -8,10 +8,51 @@ cc.Class({
     properties: {
         city_item: cc.Prefab,
         china_map: cc.Node,
-        line: cc.Graphics,
+        line_node: cc.Node,
         footer: cc.Prefab,
         header: cc.Prefab,
         layout_root: cc.Node,
+    },
+
+    drawLine(start, end) {
+        //获得组件
+        let com = this.line_node.getComponent(cc.Graphics)
+        //获得从start到end的向量
+        let line = end.sub(start)
+        //获得这个向量的长度
+        let lineLength = line.mag()
+        //设置虚线中每条线段的长度
+        let length = 20
+        //根据每条线段的长度获得一个增量向量
+        let increment = line.normalize().mul(length)
+        //确定现在是画线还是留空的bool
+        let drawingLine = true
+        //临时变量
+        let pos = start.clone()
+        com.strokeColor = cc.color(102, 136, 101);
+        com.lineWidth = 5;
+        //只要线段长度还大于每条线段的长度
+        for (; lineLength > length; lineLength -= length) {
+            //画线
+            if (drawingLine) {
+                com.moveTo(pos.x, pos.y)
+                pos.addSelf(increment)
+                com.lineTo(pos.x, pos.y)
+                com.stroke()
+            }
+            //留空
+            else {
+                pos.addSelf(increment)
+            }
+            //取反
+            drawingLine = !drawingLine
+        }
+        //最后一段
+        if (drawingLine) {
+            com.moveTo(pos.x, pos.y)
+            com.lineTo(end.x, end.y)
+            com.stroke()
+        }
     },
 
     stateUpdate() {
@@ -46,15 +87,14 @@ cc.Class({
 
         CITIES.forEach((item, index) => {
             /*画线*/
-            if (index == 0) {
-                // line_graphics.moveTo(item.positionX, item.positionY);
-            } else if (index == CITIES.length - 1) {
-                // line_graphics.lineTo(item.positionX, item.positionY);
-                // line_graphics.close();
-                // line_graphics.stroke();
-                // line_graphics.fill();
-            } else {
-                //line_graphics.lineTo(item.positionX, item.positionY);
+            if (index !== 0) {
+                let startPt = CITIES[index - 1];
+                if (!isNaN(item.middleX)) {
+                    this.drawLine(cc.v2(startPt.positionX, startPt.positionY), cc.v2(item.middleX, item.middleY));
+                    this.drawLine(cc.v2(item.middleX, item.middleY), cc.v2(item.positionX, item.positionY));
+                } else {
+                    this.drawLine(cc.v2(startPt.positionX, startPt.positionY), cc.v2(item.positionX, item.positionY));
+                }
             }
             /*生成关卡*/
             let cityItemNode = cc.instantiate(this.city_item);
