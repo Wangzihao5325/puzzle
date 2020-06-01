1cbd0c489beaf56b4b9876bfdbb6bab39134a6f9
Wangzihao5325 <416990273@qq.com>
2020-06-01 10:49:10 +0800

    1.复活弹窗增加mask

--- assets/scripts/dialog/gameFail.js
+++ assets/scripts/dialog/gameFail.js
@@ -9,21 +9,22 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        failWarp:cc.Node,
-        failContent:cc.Node,
+        failWarp: cc.Node,
+        failContent: cc.Node,
         revive: cc.Node,
-        noRevive: cc.Node
+        noRevive: cc.Node,
+        model: cc.Node,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
         //弹窗动画
-        this.failContent.setPosition(cc.v2(0,-800))
+        this.failContent.setPosition(cc.v2(0, -800))
         cc.tween(this.failContent)
-        .to(.4,{position:cc.v2(0,40)},{ easing: 'sineIn'})
-        .to(.2,{position:cc.v2(0,0)},{ easing: 'sineIn'})
-        .start()
+            .to(.4, { position: cc.v2(0, 40) }, { easing: 'sineIn' })
+            .to(.2, { position: cc.v2(0, 0) }, { easing: 'sineIn' })
+            .start()
 
         this.setTouch()
     },
@@ -34,32 +35,54 @@ cc.Class({
 
 
     setTouch(callback) {
+
         this.revive.on(cc.Node.EventType.TOUCH_START, (event) => {
+            event.stopPropagation();
+        })
+        this.revive.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
+            event.stopPropagation();
+        })
+        this.revive.on(cc.Node.EventType.TOUCH_END, () => {
             this.handleRevive()
             event.stopPropagation();
         })
+
+        this.noRevive.on(cc.Node.EventType.TOUCH_START, (event) => {
+            event.stopPropagation();
+        })
         this.noRevive.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
+            event.stopPropagation();
+        })
+        this.noRevive.on(cc.Node.EventType.TOUCH_END, () => {
             this.handleNoRevive()
             event.stopPropagation();
         })
+
+        this.model.on(cc.Node.EventType.TOUCH_START, (event) => {
+            event.stopPropagation();
+        })
+        this.model.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
+            event.stopPropagation();
+        })
+        this.model.on(cc.Node.EventType.TOUCH_END, () => {
+            event.stopPropagation();
+        })
+
     },
 
-    handleRevive(){
-        console.log("点击复活")
+    handleRevive() {
         this.failWarp.destroy()
         const menu = cc.find('Canvas/root/menuWarp').getComponent('conraol')
         menu.revive()
     },
 
-    handleNoRevive(){
-        console.log("点击取消复活")
-
+    handleNoRevive() {
         this.failWarp.destroy()
         cc.director.loadScene("mission");
 
     },
 
-    
+
 
     // update (dt) {},
 });
