ce94db83fbc3ae6b4175dff6c0a5f334ba40d5e3
Wangzihao5325 <416990273@qq.com>
2020-06-04 10:44:20 +0800

    1.游客动画优化

--- assets/scripts/show/vistor_index.js
+++ assets/scripts/show/vistor_index.js
@@ -1,9 +1,5 @@
 import { CACHE } from '../global/usual_cache';
 const SEX = { MAN: 'manVistor', WOMAN: 'womenVistor', OLD_MAN: 'oldManVistor', YOUNG_WOMEN: 'youngWomenVistor' };
-const VISTOR_ASSETS_PATH = {
-    man: ['show/nance.png', 'show/nanzheng.png'],
-    woman: ['show/nvce.png', 'show/nvzheng.png'],
-};
 const ATTITUDE_ASSET_PATH = ['show/biaoqing01', 'show/biaoqing02', 'show/biaoqing03', 'show/biaoqing04'];
 cc.Class({
     extends: cc.Component,
@@ -17,8 +13,6 @@ cc.Class({
 
     randomSexAndAttitude() {
         let manNum = Math.floor(Math.random() * 4);
-        console.log('kkkk');
-        console.log(manNum);
         switch (manNum) {
             case 0:
                 this.sex = SEX.MAN;
@@ -35,31 +29,87 @@ cc.Class({
         }
         //to do 表情随机
 
-        let index = Math.floor(Math.random() * CACHE.assets.vistorAttitude.length);
-        this.attitudeAsset = new cc.SpriteFrame(CACHE.assets.vistorAttitude[index]);
+        let attitudeNum = Math.floor(Math.random() * 10);
+        this.attitudeIndex = attitudeNum;
     },
 
     initMan(direction) {
-        if (this.sex && this.attitudeAsset) {
+        if (this.sex && (typeof this.attitudeIndex == 'number')) {
             this.man.spriteFrame = new cc.SpriteFrame(CACHE.assets[this.sex][0]);
             this.man.node.scaleX = direction > 0 ? -0.6 : 0.6;
             this.man.node.scaleY = 0.6;
+            this.attitude.spriteFrame = new cc.SpriteFrame(CACHE.assets.vistorAttitude[this.attitudeIndex]);
             this.attitudeBg.node.scaleX = 0.6;
             this.attitudeBg.node.scaleY = 0.6;
-            this.attitude.spriteFrame = this.attitudeAsset;
-            this.attitude.node.active = false;
+            switch (this.sex) {
+                case SEX.MAN:
+                    this.attitudeBg.node.x = 0;
+                    this.attitudeBg.node.y = 285;
+                    break;
+                case SEX.WOMAN:
+                    this.attitudeBg.node.x = direction > 0 ? 35 : -35;
+                    this.attitudeBg.node.y = 285;
+                    break;
+                case SEX.OLD_MAN:
+                    this.attitudeBg.node.x = direction > 0 ? -20 : 20;
+                    this.attitudeBg.node.y = 285;
+                    break;
+                case SEX.YOUNG_WOMEN:
+                    this.attitudeBg.node.x = direction > 0 ? -5 : 5;
+                    this.attitudeBg.node.y = 285;
+                    break;
+            }
+            this.attitudeBg.node.active = false;
         }
     },
 
     manWait() {
-        if (this.sex && this.attitudeAsset) {
-
-            this.man.spriteFrame = new cc.SpriteFrame(CACHE.assets[this.sex][1]);;
-            this.attitude.node.active = true;
-            this.attitude.spriteFrame = new cc.SpriteFrame(CACHE.assets.vistorAttitude[1]);;
-            this.attitudeBg.node.scaleX = 0.75;
-            this.attitudeBg.node.scaleY = 0.75;
+        if (this.sex && (typeof this.attitudeIndex == 'number')) {
+            this.man.spriteFrame = new cc.SpriteFrame(CACHE.assets[this.sex][1]);
+            this.attitudeBg.node.active = true;
         }
+    },
+
+    labelSetting(str) {
+        this.attitudeLoading.string = str;
+    },
+
+    attitudeShow() {
+        this.attitude.node.active = true;
+        let isAddHeart = false;
+        switch (this.attitudeIndex) {
+            case 0:
+                isAddHeart = true;
+                break;
+            case 1:
+                isAddHeart = false;
+                break;
+            case 2:
+                isAddHeart = true;
+                break;
+            case 3:
+                isAddHeart = true;
+                break;
+            case 4:
+                isAddHeart = false;
+                break;
+            case 5:
+                isAddHeart = false;
+                break;
+            case 6:
+                isAddHeart = false;
+                break;
+            case 7:
+                isAddHeart = true;
+                break;
+            case 8:
+                isAddHeart = false;
+                break;
+            case 9:
+                isAddHeart = true;
+                break;
+        }
+        return isAddHeart;
     },
 
     animateStart(payload) {
