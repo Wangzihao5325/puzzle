a9288fadd434fcc60f31ee0fa1146b4acec3293d
Wangzihao5325 <416990273@qq.com>
2020-06-11 10:54:20 +0800

    1.增加guide toast

---
+++ assets/scripts/guide/guideToast.js
@@ -0,0 +1,22 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        richText: cc.RichText
+    },
+
+    setContentStr(string) {
+        this.richText.string = string;
+    },
+
+    start() {
+
+    },
+});
