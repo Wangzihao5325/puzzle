18753236ca259143a8a24010e75455e9f9dd532a
louis <louislai@qq.com>
2020-06-30 14:05:31 +0800

    商城免费商品

--- assets/scripts/game/conraol.js
+++ assets/scripts/game/conraol.js
@@ -200,14 +200,14 @@ cc.Class({
         GAME_CACHE.isComplate =false;
     },
     updateUserInfo() {
-        Action.User.BalanceUpdate(this.resetUI())
+        Action.User.BalanceUpdate(this.resetUI.bind(this))
     },
 
     userProp(data, callBack) {
         const that = this;
         Api.use_prop(data, (res) => {
             if (res.code === 0) {
-                that.resetUI()
+                // that.resetUI()
                 callBack && callBack()
             }
         });
@@ -236,6 +236,7 @@ cc.Class({
         this.isViewing = !this.isViewing
         this.viewPuaaleImg.active = this.isViewing
     },
+
 
     setTouch() {
         this.magnet.on(cc.Node.EventType.TOUCH_START, (event) => {
@@ -405,7 +406,7 @@ cc.Class({
         const tiemNode = cc.find('sortTimes', this.magnet)
         const num = cc.find('time', tiemNode).getComponent(cc.Label)
         const adNode = cc.find('free', this.magnet)
-        const currentNum = userData.strongMagnet > 99 ? '99' : userData.strongMagnet
+        const currentNum = userData.strongMagnet > 99 ? userData.strongMagnet-99500 : userData.strongMagnet
         num.string = currentNum
         setTimeout(() => {
             if (userData.strongMagnet > 0) {
@@ -426,7 +427,7 @@ cc.Class({
         const tiemNode = cc.find('sortTimes', this.sort)
         const num = cc.find('time', tiemNode).getComponent(cc.Label)
         const adNode = cc.find('free', this.sort)
-        const currentNum = userData.frame > 99 ? '99' : userData.frame
+        const currentNum = userData.frame > 99 ? userData.frame-99900 : userData.frame
         num.string = currentNum
         setTimeout(() => {
             if (userData.frame > 0) {
@@ -448,7 +449,7 @@ cc.Class({
         let tiemNode = cc.find('sortTimes', this.viewIcon)
         let num = cc.find('time', tiemNode).getComponent(cc.Label)
         let adNode = cc.find('free', this.viewIcon)
-        let currentNum = userData.showProp > 99 ? '99' : userData.showProp
+        let currentNum = userData.showProp > 99 ? userData.showProp-99900 : userData.showProp
         num.string = currentNum
         setTimeout(() => {
             if (userData.showProp > 0) {
