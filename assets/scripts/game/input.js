2733e4fa42e1e6f28f7b139276e3ea2614e72f98
Wangzihao5325 <416990273@qq.com>
2020-07-08 17:39:18 +0800

    1.分享页面可以输入文字

---
+++ assets/scripts/game/input.js
@@ -0,0 +1,46 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        root: cc.Node,
+        editBox: cc.EditBox
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+    setCallback(callback) {
+        if (callback) {
+            this.inputEndCallback = callback;
+        }
+    },
+
+    onLoad() {
+        this.editBox.maxLength = 50;
+        this.root.on(cc.Node.EventType.TOUCH_START, () => {
+            event.stopPropagation();
+        });
+        this.root.on(cc.Node.EventType.TOUCH_MOVE, () => {
+            event.stopPropagation();
+        });
+        this.root.on(cc.Node.EventType.TOUCH_END, () => {
+            if (this.editBox.string && this.inputEndCallback) {
+                let string = this.editBox.string;
+                this.inputEndCallback(string);
+            }
+            this.root.active = false;
+            event.stopPropagation();
+        });
+    },
+
+    start() {
+
+    },
+
+    // update (dt) {},
+});
