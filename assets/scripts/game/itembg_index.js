5cdeaaf6251d960e3741ae799fb026a2ec285567
louis <louislai@qq.com>
2020-07-27 15:46:07 +0800

    拼图块发光

--- assets/scripts/game/itembg_index.js
+++ assets/scripts/game/itembg_index.js
@@ -5,7 +5,8 @@ cc.Class({
 
     properties: {
         mask_item: cc.Mask,
-        border: cc.Sprite
+        border: cc.Sprite,
+        borderNode:cc.Node,
     },
 
     setMarsk(index, hardLevel) {
@@ -22,18 +23,45 @@ cc.Class({
     },
 
     borderAnimate() {
-        cc.tween(this.border.node)
-            .to(0.1, { opacity: 10 })
-            .to(0.1, { opacity: 30 })
-            .to(0.1, { opacity: 80 })
-            .to(0.1, { opacity: 160 })
-            .delay(0.2)
-            .to(0.1, { opacity: 80 })
-            .to(0.1, { opacity: 30 })
-            .to(0.1, { opacity: 10 })
-            .to(0.1, { opacity: 0 })
-            .start();
+        // let material= this.borderNode.getMaterial(0);
+        // material.setProperty("spriteWidth", 30);
+        // material.setProperty("spriteHeight", 30);
+        // this.borderNode.setMaterial(0, material);
+
+        for(let i=1;i<=6;i++){
+            setTimeout(()=>{
+                this.borderWidth(i)
+            },i*30)
+        }
+
+        for(let i=6;i>=0;i--){
+            setTimeout(()=>{
+                this.borderWidth(i)
+            },(6-i)*30+200)
+        }
+        
+
+        // cc.tween(this.border.node)+++++++++++++++++++++++
+        //     .to(0.5, { opacity: 10 })
+        //     .to(0.5, { opacity: 30 })
+        //     .to(0.5, { opacity: 80 })
+        //     .to(0.3, { opacity: 255 })
+        //     .delay(0.2)
+        //     .to(0.3, { opacity: 80 })
+        //     .to(0.5, { opacity: 30 })
+        //     .to(0.5, { opacity: 10 })
+        //     .to(0.5, { opacity: 0 })
+        //     .start();
     },
+
+
+    borderWidth(width){
+        let material= this.border.getMaterial(0);
+        material.setProperty("glowRange", width);
+        this.border.setMaterial(0, material);
+        // if(with===)
+    },
+
 
     start() {
 
